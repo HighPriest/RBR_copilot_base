e5cb9c578c20590bcd417edc43df0af5d4ff3e32
HighPriest <git_user@amun.pl>
2024-10-31 02:02:22 +0100

    Overhaul Pacenotes Fixer
    + Parameters for translation & paths
    + Correctly handling creation of 2D pacenotes in "type" var
    + Pass-through of pre-existing combinations of pacenote (in flag) & type
    + Addition of pacenotes (in flag) & types, where none created
    + Correct translation of pacenotes, based on contents of flag

--- scripts/pacenotes_fixer.ps1
+++ scripts/pacenotes_fixer.ps1
@@ -1,11 +1,14 @@
-## -- This script handles two things
-# -- Removal of binds to co-driver configs (forces use of default RBR.ini)
-# -- Adds 2D/3D Pacenotes & fixes broken flag IDs.
+param (
+    [Parameter(Mandatory = $false, HelpMessage = "Do you want to execute translation based on the provided map? DEFAULT: FALSE")]
+    [bool]$translate_flag = $false,
 
-## -- To use it, you are going to need to handle two things
-# -- In $pacenotes_map table, match pacenote ID's (from your codrivers config), to names of flags, which are specified in $flag_map.
-# -- How the flags look, can be viewed in .\RBR\Generic\PC_1024_01.dds
-# -- In $pacenotesDirectory & $outputFilePath, set the MyPacenotes directory with your pacenotes & where you want to put the exports.
+    # Path to the input & output directories
+    [Parameter(Mandatory = $false, HelpMessage = "Source directory with foldered pacenotes DEFAULT: '.\Pacenotes'")]
+    [string]$pacenotesDirectory = ".\MyPacenotes",
+
+    [Parameter(Mandatory = $false, HelpMessage = "Target directory for the converted pacenotes DEFAULT: '.\Pacenotes_Fixed'")]
+    [string]$outputFilePath = ".\MyPacenotes_Fixed"
+)
 
 # Ensure the ini module is installed
 try {
@@ -17,12 +20,25 @@ catch {
     Import-Module PsIni
 }
 
-# Path to the input & output directories
-$pacenotesDirectory = "SOURCE_DIRECTORY"
-$outputFilePath = "TARGET_DIRECTORY"
+
 
 # Get list of all stages directories
 $stages = Get-ChildItem -Path $pacenotesDirectory -Directory
+
+# Map between old standard of pacenotes & new standard
+try {
+    $ids_map = @{}
+
+    $csvData = Import-Csv -Path '.\MAP_CoDrivers.csv'
+
+    foreach ($row in $csvData) {
+        $ids_map[[int32]$row.id_old] = [int32]$row.id_new;
+    } # Unwrapping the CSV list into a hashmap
+} catch {
+    $translate_flag = $false
+    Write-Output "TRANSLATE MAP NOT FOUND! NOT TRANSLATING!"
+}
+
 
 # Mapping of my pacenote id's to flags
 $pacenotes_map = @{
@@ -48,7 +64,7 @@ $pacenotes_map = @{
 107 = "HAIRPINLEFT"; #LEFT_DONUT
 108 = "FASTLEFT"; #LEFT_SHORT
 109 = "FASTLEFT"; #LEFT_OVER
-# 110 = ; #CHICANE_LEFT_ENTRY # TODO: Make it twisty
+110 = "90_RIGHT"; #CHICANE_LEFT_ENTRY # TODO: Make it twisty
 
 112 = "EASYRIGHT"; #RIGHT # TODO: Check if the image fits
 113 = "FLATRIGHT"; #RIGHT_6
@@ -60,12 +76,12 @@ $pacenotes_map = @{
 119 = "HAIRPINLEFT"; #RIGHT_DONUT
 120 = "FASTRIGHT"; #RIGHT_SHORT
 121 = "FASTLEFT"; #RIGHT_OVER
-# 122 = ; #CHICANE_RIGHT_ENTRY # TODO: Make it twisty
+122 = "90_LEFT"; #CHICANE_RIGHT_ENTRY # TODO: Make it twisty
 
 }
 
 # Mapping of corner names to original ID's
-$flags_map = @{
+$standard_map = @{
     "HAIRPINLEFT" = 0;
     "90_LEFT" = 1;
     "KLEFT" = 2;
@@ -82,13 +98,15 @@ $flags_map = @{
     "EASYRIGHT" = 6;
     "FLATRIGHT" = 25;
 
-    # "CAUTION" = ??;
-    # "WATER" = ??;
-    # "TIGHTENS" = ??;
-    # "WIDENS" = ??;
-    # "BRIDGE" = ??;
-    # "BUMP" = ??;
-    # "TWISTY" = ??;
+    "CAUTION" = 32; # Red triangle
+    "CARE" = 18; # Orange triangle
+    "WATER" = 17;
+    "NARROWS" = 14;
+    "WIDENS" = 15;
+    "BRIDGE" = 27;
+    "BUMP" = 19;
+    "TWISTY" = 12;
+    "OVER_CREST" = 16;
 }
 
 # Iterate over all stages directories
@@ -97,19 +115,19 @@ foreach ($stage in $stages) {
     $pacenotes = Get-ChildItem -Path $stage.FullName -File
 
     # Iterate over individual pacenote files
-    foreach ($pacenote in $pacenotes) {
-        if (Test-Path ($outputFilePath + "/" + $stage.Name + "/" + $pacenote.Name)) {
+    foreach ($file in $pacenotes) {
+        if (Test-Path ($outputFilePath + "/" + $stage.Name + "/" + $file.Name)) {
             continue
         }
         # Load contents of the original INI file
         $iniData = [ordered] @{}
-        $iniData = Import-Ini $pacenote.FullName
+        $iniData = Import-Ini $file.FullName
 
         # Prepare structures for new INI file
         if ($iniData.Contains("PACENOTES")) {
             $filteredData = [ordered] @{
                 _ = [ordered]@{
-                    Comment1 = ($pacenote.Name + " pacenotes file generated for CoPilot mods compatible with FilipekMod.")
+                    Comment1 = ($file.Name + " pacenotes file generated for CoPilot mods compatible with FilipekMod.")
                     Comment2 = ("For more details, please visit https://amun.pl/blog")
                 }
                 PACENOTES = $iniData["PACENOTES"]
@@ -117,7 +135,7 @@ foreach ($stage in $stages) {
         } else {
             $filteredData = [ordered] @{
                 _ = [ordered]@{
-                    Comment1 = ($pacenote.Name + " pacenotes file generated for CoPilot mods compatible with FilipekMod.")
+                    Comment1 = ($file.Name + " pacenotes file generated for CoPilot mods compatible with FilipekMod.")
                     Comment2 = ("For more details, please visit https://amun.pl/blog")
                 }
                 PACENOTES = [ordered] @{
@@ -137,15 +155,47 @@ foreach ($stage in $stages) {
                 continue
             }
             else {
+                $pacenote = 0
+                $distance = 0
+                $standard = 0 # The 2D Pacenote
+                $flags = 0
+
+                if ([int32]$iniData[$section].flag -band 0b100000000000000) {
+                    $pacenote = ([int32]$iniData[$section].flag -shr 19)
+                    $distance = [int32]$iniData[$section].distance
+                    $standard = [int32]$iniData[$section].type
+                    $flags = ([int32]$iniData[$section].flag -band 256)
+                } else {
+                    $pacenote = [int32]$iniData[$section].type
+                    $distance = [int32]$iniData[$section].distance
+                    $standard = $null
+                    $flags = ([int32]$iniData[$section].flag -band 256)
+                }
+
                 $filteredData["P$pCounter"] = $iniData[$section]
-                if ($pacenotes_map.ContainsKey([int32]$filteredData["P$pCounter"].type)) {
-                    $note_id = $flags_map[ # Get the flag ID from flags_map
+                if ($translate_flag) { # Check if user has indicated desire to execute translation
+                    if($ids_map.ContainsKey([int32]$pacenote)) {
+                        # Debug
+                        #if(-not $ids_map[[int32]$pacenote].Equals([int32]$pacenote)) {
+                        #    Write-Output "Translating: $($pacenote) To: $($ids_map[[int32]$pacenote])"
+                        #}
+                        [int32]$pacenote = $ids_map[[int32]$pacenote] # Translate the pacenotes
+                    }
+                }
+                if ($null -ne $standard) {
+                    # Set the flag to Pacenote_id bitwise shifted left + 0b100000000000000 + ignore flag at 8th binary 2^8
+                    $filteredData["P$pCounter"].flag = ([int32]([int32]$pacenote -shl 19)) + 0b100000000000000 + ([int32]$flags -band 256)
+                    $filteredData["P$pCounter"].type = $standard
+                    # Only make sure the flag is either 0 or 256
+                    #$filteredData["P$pCounter"].flag = ([int32]$filteredData["P$pCounter"].flag -band 256)
+                } elseif ($pacenotes_map.ContainsKey([int32]$pacenote)) {
+                    $standard = $standard_map[ # Get the standard ID from standard_map
                         $pacenotes_map[ # Get the og Pacenote name from pacenotes_map
-                           [int32]$filteredData["P$pCounter"].type # Get the pacenote ID
+                            [int32]$pacenote # Get the pacenote ID
                         ]
                     ]
-                    # Set the flag to Pacenote_id bitwise shifted left + 0b100000000000000 + ignore flag at 8th binary 2^8
-                    $filteredData["P$pCounter"].flag = ([int32]($note_id -shl 19)) + 0b100000000000000 + ([int32]$filteredData["P$pCounter"].flag -band 256)
+                    $filteredData["P$pCounter"].flag = ([int32]([int32]$pacenote -shl 19)) + 0b100000000000000 + ([int32]$flags -band 256)
+                    $filteredData["P$pCounter"].type = $standard
                 } else {
                     $filteredData["P$pCounter"].flag = [string]([int32]$filteredData["P$pCounter"].flag -band 256) # Only check for the "ignore link" flag
                 }
@@ -154,18 +204,14 @@ foreach ($stage in $stages) {
             }
         }
 
-        if (-not (Test-Path -Path ($outputFilePath + "/" + $stage.Name))) {
-            New-Item -Path ($outputFilePath + "/" + $stage.Name) -ItemType Directory
-            Write-Output "Directory created: ($outputFilePath + "/" + $stage.Name)"
+        if (-not (Test-Path -Path ($outputFilePath + "\" + $stage.Name))) {
+            New-Item -Path ($outputFilePath + "\" + $stage.Name) -ItemType Directory
+            Write-Output "Directory created: $($outputFilePath + "\" + $stage.Name)"
         }
 
         $filteredData["PACENOTES"].count = $pCounter
-        Export-Ini -Format "pretty" -InputObject $filteredData -Encoding ASCII ($outputFilePath + "/" + $stage.Name + "/" + $pacenote.Name)
+        Export-Ini -Format "pretty" -InputObject $filteredData -Encoding ASCII ($outputFilePath + "\" + $stage.Name + "\" + $file.Name)
 
         Write-Output "INI file has been rebuilt successfully."
     }
-}
-
-function TranslatePacenote ($pacenote_file, $hashmap_of_values) {
-    # TODO: Implement this based on provided maps
 }
